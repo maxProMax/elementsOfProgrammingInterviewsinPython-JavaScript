f58d20ed77c778a1c93e5f352b6eeaa25bc9f526
maxProMax <thisismaxproemail@gmail.com>
2020-10-21 13:59:03 +0300

    add multiply, divide, power, reverse

--- Part_II/Data_Structures_and_Algoritms/PrimitiveTypes.js
+++ Part_II/Data_Structures_and_Algoritms/PrimitiveTypes.js
@@ -47,8 +47,98 @@ function closestIntSameBitCount(x) {
     throw 'All bits are 0 or 1';
 }
 
-console.log(logResult(closestIntSameBitCount, 7));
+// console.log(logResult(closestIntSameBitCount, 7));
 
 function logResult(fn, ...args) {
     return `${fn.name} - input=${args[0]}, inputBin=${(args[0]).toString(2)}. Result=${fn(...args)}`;
 }
+
+function multiply(x, y) {
+    function add(a, b) {
+        let runningSum = 0;
+        let carryin = 0;
+        let k = 1;
+        let temp_a = a;
+        let temp_b = b;
+
+        while (temp_a || temp_b) {
+            var ak = a & k;
+            var bk = b & k;
+            var carryout = (ak & bk) | (ak & carryin) | (bk & carryin);
+            runningSum = runningSum | (ak ^ bk ^ carryin);
+            carryin = carryout << 1;
+            k = k << 1;
+            temp_a = temp_a >> 1;
+            temp_b = temp_b >> 1;
+        }
+
+        return runningSum | carryin;
+    }
+
+    let runningSum = 0;
+
+    while (x) {
+        if (x & 1) {
+            runningSum = add(runningSum, y);
+        }
+        x = x >> 1;
+        y = y << 1;
+    }
+
+    return runningSum;
+}
+
+// console.log('multiply', multiply(10, 5));
+
+function divide(x, y) {
+    let result = 0;
+    const getBaseLog = (x, y) =>  Math.ceil(Math.log(y) / Math.log(x));
+    let power = 30 - getBaseLog(2, y);
+
+    let yPower = y << power;
+
+    while (x >= y) {
+        while (yPower > x) {
+            yPower = yPower >> 1;
+            power -= 1;
+        }
+
+        result += 1 << power;
+        x -= yPower;
+    }
+
+    return result;
+}
+
+// console.log('divide', divide(500, 10));
+
+function power(x, y) {
+    var result = 1;
+    var power = y;
+
+    while (power) {
+        if (power & 1) {
+            result *= x;
+        }
+        x = x * x;
+        power = power >> 1;
+    }
+
+    return result;
+}
+
+// console.log('power', power(2,10));
+
+function reverse(x) {
+    var xRemaining = x;
+    var result = 0;
+
+    while (xRemaining) {
+        result = result * 10 + xRemaining % 10;
+        xRemaining = Math.floor(xRemaining / 10)
+    }
+
+    return result;
+}
+
+// console.log('reverse', reverse(5432));
--- Part_II/Data_Structures_and_Algoritms/Primitive_Types.py
+++ Part_II/Data_Structures_and_Algoritms/Primitive_Types.py
@@ -1,3 +1,5 @@
+import math
+
 getStrResult = lambda fn, *args: f'{fn.__name__} - input={args[0]}, input_bin={bin(args[0])}. Result={fn(*args)}'
 
 def count_of_bits(x):
@@ -34,4 +36,79 @@ def closest_int_same_bit_count(x):
             return x
     raise ValueError('All bits are 0 or 1')
 
-print(getStrResult(closest_int_same_bit_count, 7))
+# print(getStrResult(closest_int_same_bit_count, 7))
+
+def multiply(x,y):
+    def add(a, b):
+        running_sum = 0
+        carryin = 0
+        k = 1
+        temp_a = a
+        temp_b = b
+
+        while temp_a or temp_b:
+            ak = a & k
+            bk = b & k
+            carryout = (ak & bk) | (ak & carryin) | (bk & carryin)
+            running_sum |= ak ^ bk ^ carryin
+            carryin = carryout << 1
+            k = k << 1
+            temp_a = temp_a >> 1
+            temp_b = temp_b >> 1
+
+        return running_sum | carryin
+
+    running_sum = 0
+    while x:
+        if x & 1:
+            print(running_sum, y, add(running_sum, y))
+            running_sum = add(running_sum, y)
+
+        x = x >> 1
+        y = y << 1
+
+    return running_sum
+
+# print('multiply', multiply(10, 5))
+
+def divide(x, y):
+    result = 0
+    power = 32
+    y_power = y << power
+
+    while x >= y:
+        while y_power > x:
+            y_power >>= 1
+            power -= 1
+        result += 1 << power
+        x -= y_power
+    return result
+
+# print('divide', divide(29, 2))
+
+def power(x, y):
+    result = 1
+    power = y
+
+    while power:
+        if power & 1:
+            result *= x
+
+        x = x * x
+        power = power >> 1
+
+    return result
+
+# print(power(2,10))
+
+def reverse(x):
+    result = 0
+    x_remaining = x
+
+    while x_remaining:
+        result = result * 10 + x_remaining % 10
+        x_remaining = math.floor(x_remaining / 10)
+
+    return -result if x < 0 else result
+
+# print('reverse', reverse(5432))
